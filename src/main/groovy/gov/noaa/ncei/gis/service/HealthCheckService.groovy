cf94ae5b388e2a50365f63f15ae86bab9ceab404
John Cartwright <john.c.cartwright@comcast.net>
2016-07-04 17:08:22 -0600

    setup scheduled checks and add methods for executing checks

--- src/main/groovy/gov/noaa/ncei/gis/service/HealthCheckService.groovy
+++ src/main/groovy/gov/noaa/ncei/gis/service/HealthCheckService.groovy
@@ -1,15 +1,33 @@
 package gov.noaa.ncei.gis.service
 
+import groovy.util.logging.Log4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import gov.noaa.ncei.gis.domain.*
 
+@Log4j
 @Service
 class HealthCheckService {
+    private HealthCheckRepository healthCheckRepository
 
-    void runCheck(Long id) {
-
+    @Autowired
+    HealthCheckService(HealthCheckRepository healthCheckRepository) {
+        this.healthCheckRepository = healthCheckRepository
     }
 
-    void runChecks() {
 
+    void runCheck(Long id) {
+        log.info "running check for HealthCheck ${id}..."
+    }
+
+
+    void runChecks(List<HealthCheck> checks) {
+        if (checks.size() == 0) {
+            log.warn "Empty list of HeathChecks received"
+            return
+        }
+        checks.each {
+            runCheck(it.id)
+        }
     }
 }
--- src/main/groovy/gov/noaa/ncei/gis/service/ScheduledTasksService.groovy
+++ src/main/groovy/gov/noaa/ncei/gis/service/ScheduledTasksService.groovy
@@ -1,33 +1,78 @@
 package gov.noaa.ncei.gis.service
 
+import groovy.util.logging.Log4j
+import org.springframework.beans.factory.annotation.Autowired
 import java.text.SimpleDateFormat;
-//import java.util.Date;
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import gov.noaa.ncei.gis.domain.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
+@Log4j
 @Component
 public class ScheduledTasksService {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss")
+    private HealthCheckService healthCheckService
+    private HealthCheckRepository healthCheckRepository
+
+    @Autowired
+    ScheduledTasksService(HealthCheckService healthCheckService, HealthCheckRepository healthCheckRepository) {
+        this.healthCheckService = healthCheckService
+        this.healthCheckRepository = healthCheckRepository
+    }
 
     @Scheduled(fixedRate = 5000L)
     public void reportCurrentTime() {
-        println("The time is now " + dateFormat.format(new Date()))
+        println("The time is now ${dateFormat.format(new Date())}")
     }
+
+
+    @Scheduled(cron="0 */5 * * * *")
+    public void checkEvery5Minutes() {
+        List<HealthCheck> checks = healthCheckRepository.findByCheckInterval(CheckIntervalEnum.FIVEMINUTES)
+        if (checks.size == 0) {
+            log.info("no 5 minute interval checks to run")
+            return
+        }
+
+        healthCheckService.runChecks(checks)
+    }
+
 
     @Scheduled(cron="0 */15 * * * *")
     public void checkEvery15Minutes() {
-        println("This is your 15 minute check. The time is now " + dateFormat.format(new Date()))
+        List<HealthCheck> checks = healthCheckRepository.findByCheckInterval(CheckIntervalEnum.FIFTEENMINUTES)
+        if (checks.size == 0) {
+            log.info("no 15 minute interval checks to run")
+            return
+        }
+
+        healthCheckService.runChecks(checks)
     }
+
 
     @Scheduled(cron="0 0 * * * *")
     public void checkHourly() {
-        println("This is your hourly check. The time is now " + dateFormat.format(new Date()))
+        List<HealthCheck> checks = healthCheckRepository.findByCheckInterval(CheckIntervalEnum.HOURLY)
+        if (checks.size == 0) {
+            log.info("no hourly checks to run")
+            return
+        }
+
+        healthCheckService.runChecks(checks)
     }
 
     @Scheduled(cron="0 0 0 * * *")
     public void checkDaily() {
-        println("This is your daily check. The time is now " + dateFormat.format(new Date()))
+        List<HealthCheck> checks = healthCheckRepository.findByCheckInterval(CheckIntervalEnum.DAILY)
+        if (checks.size == 0) {
+            log.info("no daily checks to run")
+            return
+        }
+
+        healthCheckService.runChecks(checks)
     }
 
 }
