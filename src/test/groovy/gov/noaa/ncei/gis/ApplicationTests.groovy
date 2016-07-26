b049f434b2745fe436c1f2c292db15e6762f2001
John Cartwright <john.c.cartwright@comcast.net>
2016-07-25 22:23:41 -0600

    enhancement to ApplicationTest, complete rewrite of HealthCheckControllerTest

--- src/test/groovy/gov/noaa/ncei/gis/ApplicationTests.groovy
+++ src/test/groovy/gov/noaa/ncei/gis/ApplicationTests.groovy
@@ -1,18 +1,31 @@
 package gov.noaa.ncei.gis
 
+import gov.noaa.ncei.gis.domain.HealthCheck
+import gov.noaa.ncei.gis.domain.HealthCheckRepository
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = Application)
-@WebAppConfiguration
+//@WebAppConfiguration
 class ApplicationTests {
+
+	@Autowired
+    HealthCheckRepository healthCheckRepository
+
+    @Autowired
+    JdbcTemplate jdbcTemplate
 
 	@Test
 	void contextLoads() {
+        Assert.assertNotNull("the HealthCheck repository should be non-null", this.healthCheckRepository)
+        Assert.assertNotNull("the JdbcTemplate should be non-null", this.jdbcTemplate)
 	}
 
 }
--- src/test/groovy/gov/noaa/ncei/gis/web/HealthCheckControllerTest.groovy
+++ src/test/groovy/gov/noaa/ncei/gis/web/HealthCheckControllerTest.groovy
@@ -1,122 +1,124 @@
 package gov.noaa.ncei.gis.web
 
-import com.jayway.restassured.http.ContentType
 import gov.noaa.ncei.gis.Application
-import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.boot.test.WebIntegrationTest
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.transaction.TransactionConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.client.RestTemplate
+import org.springframework.http.MediaType
+import java.nio.charset.Charset
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static org.hamcrest.Matchers.*
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
+import org.springframework.security.test.context.support.*
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import com.jayway.restassured.response.Response;
-import com.jayway.restassured.RestAssured
-import com.jayway.restassured.specification.RequestSpecification;
-//import static org.hamcrest.Matchers.*
-import org.hamcrest.Matchers
-import groovy.json.*
 
 
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = Application)
-@WebIntegrationTest(randomPort = true)
+@WebAppConfiguration
+//@TransactionConfiguration(defaultRollback = true)
+@Transactional
 class HealthCheckControllerTest {
-    @Value('${local.server.port}')
-    private port;
+    @Autowired
+    private WebApplicationContext webApplicationContext
 
-    @Value('${security.user.password}')
-    private password
 
-    @Value('${security.user.name}')
-    private username
-
-    private url
-
-    private JsonSlurper jsonSlurper = new JsonSlurper()
+    private RestTemplate restTemplate
+    private MockRestServiceServer mockServer
+    private MockMvc mockMvc
+    private MediaType applicationJsonMediaType =
+            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
     @Before
     void setup() {
-        this.url = "http://localhost:${this.port}/healthChecks"
+//        List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+//        converters.add(new StringHttpMessageConverter());
+//        converters.add(new MappingJackson2HttpMessageConverter());
+
+
+        this.restTemplate = new RestTemplate()
+//        this.restTemplate.setMessageConverters(converters);
+        this.mockServer = MockRestServiceServer.createServer(this.restTemplate)
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).apply(springSecurity()).build()
     }
 
     @Test
-    public void testGetHealthChecks() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        ResponseEntity<String> entity = new TestRestTemplate().exchange(
-                url, HttpMethod.GET,
-                new HttpEntity<Void>(headers), String.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    public void testGetAllHealthChecks() {
+        this.mockMvc.perform(get("/healthChecks"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(applicationJsonMediaType))
+                .andExpect(jsonPath('$', hasSize(3)))
 
-        def json = jsonSlurper.parseText(entity.body)
-        println json
+        //println this.mockMvc.perform(get("/healthChecks")).andReturn().getResponse().contentAsString
     }
+
 
     @Test
-    public void getHealthChecks() {
-        final Response response = RestAssured.given()
-                .accept(ContentType.JSON)
-                .get(url)
-        assertThat(response.getStatusCode(), Matchers.equalTo(200))
-        //TODO compare JSON content        println response.body
+//    @WithMockUser(username = 'admin', password = 'mypassword', roles = ['ADMIN'])
+    public void testCreateHealthChecks() {
+        String jsonBody = '{ "url": "http://www.example.com" }'
+
+        MvcResult mvcResult = mockMvc.perform(post("/healthChecks")
+                .with(httpBasic('admin','mypassword'))
+                .content(jsonBody).contentType(applicationJsonMediaType))
+                .andExpect(status().isCreated())
+                .andReturn()
+
+        assert mvcResult.getResponse().getHeader('Location')
+
+        mockMvc.perform(get("/healthChecks"))
+                .andExpect(jsonPath('$', hasSize(4)))
     }
 
-    @Test
-    public void testCreateHealthCheckWithValidCredentials() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        ResponseEntity<String> entity = new TestRestTemplate(username, password).exchange(
-                url, HttpMethod.POST,
-                new HttpEntity<Void>(headers), String.class);
-        assertEquals(HttpStatus.CREATED, entity.getStatusCode());
-    }
 
     @Test
-    public void createHealthCheckWithInvalidCredentials() {
-        final Response response = RestAssured.given()
-                .auth()
-                .preemptive()
-                .basic(username, 'badpassword')
-                .accept(ContentType.JSON)
-                .post(url)
-        assertThat(response.getStatusCode(), Matchers.equalTo(401))
+    public void testCreateHealthChecksWithoutCredentials() {
+        String jsonBody = '{ "url": "http://www.example.com" }'
+
+        mockMvc.perform(post("/healthChecks")
+                .content(jsonBody).contentType(applicationJsonMediaType))
+                .andExpect(status().isUnauthorized())
+
+        mockMvc.perform(get("/healthChecks"))
+                .andExpect(jsonPath('$', hasSize(3)))
     }
 
+
     @Test
-    public void createHealthCheckWithoutCredentials() {
-        final Response response = RestAssured.given()
-                .accept(ContentType.JSON)
-                .post(url)
-        assertThat(response.getStatusCode(), Matchers.equalTo(401))
+    public void testCreateHealthChecksWithInvalidCredentials() {
+        String jsonBody = '{ "url": "http://www.example.com" }'
+
+        mockMvc.perform(post("/healthChecks")
+                .with(httpBasic('admin','badpassword'))
+                .content(jsonBody).contentType(applicationJsonMediaType))
+                .andExpect(status().isUnauthorized())
+
+        mockMvc.perform(get("/healthChecks"))
+                .andExpect(jsonPath('$', hasSize(3)))
     }
+
 }
